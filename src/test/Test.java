92c69ca3327d5a59b91bf48a748b4e1ce8e142de
yangsehyun0910 <yangsehyun1@gmail.com>
2024-02-22 23:02:05 +0900

    출력 시스템에러 해결

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,9 +3,9 @@ package test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(aaa);
-        System.out.println(한번더);
-        System.out.println(커밋만으로 잔디가?);
-        System.out.println(확신이야);
+        System.out.println("aaa");
+        System.out.println("한번더");
+        System.out.println("커밋만으로 잔디가?");
+        System.out.println("확신이야");
     }
 }
