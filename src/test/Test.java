297c600916a5a86199c507f88c139fd807b7d0d4
yangsehyun0910 <yangsehyun1@gmail.com>
2024-02-22 22:40:08 +0900

    커밋만으로 잔디가 심길까?

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(aaa);
         System.out.println(한번더);
+        System.out.println(커밋만으로 잔디가?);
     }
 }
