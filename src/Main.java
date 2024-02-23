2a850b7580585126a344ace872d32a69a3e18531
yangsehyun0910 <yangsehyun1@gmail.com>
2024-02-23 17:13:47 +0900

    1단계 11번 실패

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*
+        꼬마 정민이는 이제 A + B 정도는 쉽계 계산할 수 있다.
+        이제 A + B + C를 계산할 차례이다!
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        첫 번째 줄에 A, B, C( 1<= A, B, C <=10의 12승)이 공백을 사이에 두고 주어진다.
+        A + B + C이 값을 출력한다.
+
+        입력 예시
+        77 77 7777
+
+        출력 예시
+        7931
+        */
+
+        Scanner scanner = new Scanner(System.in);
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+        System.out.println(A + " " + B + " " + C);
+
+        long sum = A + B + C;
+        System.out.println(sum);
     }
 }
