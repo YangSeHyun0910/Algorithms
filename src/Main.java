dfdaa5e7ff062ad4c86a579ff1fa84dc1f62f83e
yangsehyun0910 <yangsehyun1@gmail.com>
2024-02-28 19:57:00 +0900

    3단계 9번:별 찍기1, 10번:별 찍기2

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
-        int[] count = new int[t];
-        String[] calc = new String[t];
+        int n = scanner.nextInt();
 
-        for (int i = 0, a, b; i < t; i++) {
-            a = scanner.nextInt();
-            b = scanner.nextInt();
-            if (a > 0 && a < 10 && b > 0 && b < 10) {
-                calc[i]= a + " + " + b ;
-                count[i] = a + b;
+        if (n > 0 && n < 101) {
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    System.out.print(" ");
+                }
+                for (int q = 0; q < i + 1; q++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-        }
-
-        for (int i = 0; i < count.length; i++) {
-            System.out.println("Case #" + (i+1) + ": " + calc[i] + " = " + count[i]);
         }
     }
 }
