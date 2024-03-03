daa8c7c983b4c432c1bd7e03d5f5c77549f619a3
yangsehyun0910 <yangsehyun1@gmail.com>
2024-03-03 22:49:08 +0900

    4단계 3번:최소,최대

--- src/stage4/Stage4_3.java
+++ src/stage4/Stage4_3.java
@@ -1,13 +1,33 @@
-import java.io.*;
+package stage4;
+
 import java.util.Scanner;
 
+public class Stage4_3 {
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        /*
+        문제 - 최소, 최대
+        N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+        입력
+        첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다.
+        둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다.
+        모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+        출력
+        첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+
+        예제 입력 1
+        5
+        20 10 35 30 7
+
+        예제 출력 1
+        7 35
+        */
         Scanner scanner = new Scanner(System.in);
         int n = 0; //입력받을 정수의 갯수
-        int max = -1000001; //최대값 담아 둘 변수
-        int min = 1000001; //최소값 담아 둘 변수
+        int max = 0; //최대값 담아 둘 변수
+        int min = 1000000; //최소값 담아 둘 변수
         n = scanner.nextInt();
         int[] array = new int[n]; //입력받은 정수를 담아 둘 배열
 
@@ -24,7 +44,10 @@ public class Main {
 
                 //-1,000,000 <= a <= 1,000,000 라면 배열에 담는다
                 array[i] = a;
+            }
 
+            //최소값, 최대값 구하는 반복문
+            for (int i = 0; i < n; i++) {
                 if (min > array[i]) {
                     min = array[i];
                 }
@@ -32,7 +55,9 @@ public class Main {
                     max = array[i];
                 }
             }
+
             System.out.println(min + " " + max);
         }
+
     }
 }
