b3329a03120db8d50858a8b4356ec64efab82b1d
yangsehyun0910 <yangsehyun1@gmail.com>
2024-03-09 17:01:49 +0900

    4단계 8번:나머지

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        // 출석부 총 번호를 arr[30]으로 선언
-        // 레포트를 제출한 출석번호 n선언
-        // 레포트를 제출한 출석번호를 담는 배열 submit선언
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                21, 22 ,23, 24, 25, 26, 27, 28, 29, 30};
-        int[] submit = new int[arr.length];
-        int n = 0;
+        // 나머지가 서로 같지 않은 수의 갯수 count 선언
+        int[] arr = new int[10];
+        int count = 10;
 
-        for (int i = 0; i < 28; i++) {
+        for (int i = 0, n = 0; i < arr.length; i++) {
             n = scanner.nextInt();
-            if (n >= 1 && n <= 30) {
-                for (int j = 0; j < arr.length; j++) {
-                    if (arr[j] == n) {
-                        submit[j] = arr[j];
-                    }
-                }
+            if (n > 0 && n <= 1000) {
+                arr[i] = n%42;
             } else {
                 break;
             }
         }
-
-        for (int i = 0; i < submit.length; i++) {
-            if (submit[i] != arr[i]) {
-                System.out.println(arr[i]);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] != arr[j]) {
+                    continue;
+                } else {
+                    count -= 1;
+                    break;
+                }
             }
         }
+
+        System.out.println(count);
     }
 }
