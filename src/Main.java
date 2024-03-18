54c1478b9aa43ee758fa142c802fcca36658fe35
yangsehyun0910 <yangsehyun1@gmail.com>
2024-03-18 17:07:09 +0900

    5단계 5번:숫자의 합

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        char a = scanner.nextLine().charAt(0);
-        int ascii = (int)a;
-        System.out.println(ascii);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        String s = scanner.nextLine();
+        int sum = 0;
+
+        if (n >= 1 && n <= 100) {
+            for (int i = 0; i < n; i++) {
+                sum += s.charAt(i) - '0';
+            }
+        }
+
+        System.out.println(sum);
     }
 }
