70f01f79647939c3b76b5d978c026f040fb164c9
yangsehyun0910 <yangsehyun1@gmail.com>
2024-04-02 20:07:58 +0900

    6단계 3번:별 찍기-7 완성.

--- src/Main.java
+++ src/Main.java
@@ -5,64 +5,55 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        int[] arr = new int[6];
-        int[] count = new int[6];
+        int n = scanner.nextInt();
 
-        //각각의 체스말의 갯수를 입력받기 위해 6번 반복 실행
-        for (int i = 0, a = 0; i < 6; i++) {
-            a = scanner.nextInt();
-            if (a >= 0 && a <= 10) {
-                arr[i] = a;
-                count[i] = 0;
-            } else {
-                break;
-            }
-        }
+        //1. 총 2*n-1 번 만큼 반복을 해야한다
+        //2. 입력받은 n을 기준으로 미만=오름차순, 초과=내림차순으로 별 출력.
+        if (n >= 1 && n <= 100) {
+            //입력받은 수 만큼 n번 만큼 줄을 오름차순으로 출력 후, n+1번 줄부터는 내림차순으로 출력한다 (대칭)
+            //=>(n*2)-1 만큼 반복을 시킨다
+            for (int i = 0; i < (2 * n) - 1; i++) {
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i <= 1) {
-                    if (arr[i] == 1 || count[i] == 1) {
-                        break;
-                    } else if (arr[i] >= 2 && arr[i] <= 10) {
-                        count[i] = count[i] - 1;
-                        arr[i] = arr[i] - 1;
-                        continue;
-                    } else if (arr[i] <= 0) {
-                        count[i] = count[i] + 1;
-                        arr[i] = arr[i] + 1;
-                        continue;
-                    }
-                } else if (i >= 2 && i <= 4) {
-                    if (arr[i] == 2 || count[i] == 2) {
-                        break;
-                    } else if (arr[i] >= 3 && arr[i] <= 10) {
-                        count[i] = count[i] - 1;
-                        arr[i] = arr[i] - 1;
-                        continue;
-                    } else if (arr[i] < 2) {
-                        count[i] = count[i] + 1;
-                        arr[i] = arr[i] + 1;
-                        continue;
-                    }
-                } else {
-                    if (arr[i] == 8 || count[i] == 8) {
-                        break;
-                    } else if (arr[i] >= 9 && arr[i] <= 10) {
-                        count[i] = count[i] - 1;
-                        arr[i] = arr[i] - 1;
-                        continue;
-                    } else if (arr[i] < 8) {
-                        count[i] = count[i] + 1;
-                        arr[i] = arr[i] + 1;
-                        continue;
+                //오름차순 출력의 기준을 n으로.
+                if (i < n) {
+
+                    //줄당 출력 갯수가, 첫번째 줄=n, 두번째 줄=n+1, 세번째 줄=n+2....
+                    //한 줄 늘어날 때 마다, 해당 줄의 출력 갯수가 1개씩 증가 하기 때문에 종료시점을 j < n+i
+                    for (int j = 0; j < n + i; j++) {
+
+                        //공백 + 별(*) 순서이기 때문에
+                        //공백 공백 공백 별... 이 출력될 수 있도록 조건설정
+                        if (j + i < n - 1) {
+                            System.out.print(" ");
+                        } else if (j + i >= n - 1) {
+                            System.out.print("*");
+                        }
                     }
                 }
-            }
-        }
 
-        for (int i = 0; i < count.length; i++) {
-            System.out.print(count[i] + " ");
+                //출력 줄 수가 입력값 n보다 크거나 같아 졌을때 내림차순으로 반전되어야 한다.
+                if (i >= n) {
+
+                    //내림차순이기 때문에 시작값을 (2 * n) - 1 으로 설정
+                    for (int j = (2 * n) - 1; j > 1; j--) {
+
+
+                        if (j - i + (n - 1) > 0) {
+
+                            //공백 + 별(*) 순서로 출력되어야한다.
+                            //또한, 공백의 수가 1,2,3... 으로 증가된다.
+                            //고정값=n, 증가=i, 감소=j
+                            //고정값 n을 활용해서 변동값인 i, j을 통해 조건 성립.
+                            if ((n * 2) - i + (n - 1) <= j) {
+                                System.out.print(" ");
+                            } else if (j - i + (n - 1) >= n - (n - 1)) {
+                                System.out.print("*");
+                            }
+                        }
+                    }
+                }
+                System.out.println();
+            }
         }
     }
 }
