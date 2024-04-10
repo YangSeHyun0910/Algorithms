f3390b75912e7f9bb1485a485e7466809447485f
yangsehyun0910 <yangsehyun1@gmail.com>
2024-04-10 17:39:17 +0900

    6단계 5번:단어 공부. 시간초과 해결완료.

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,66 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        /*
+        1.문자열을 입력받는다.
+        2.입력받은 문자열을 대문자로 전환.
+        3.배열의 크기가 26(알파벳 갯수)인 int타입의 배열 선언
+        4.문자열에 해당 문자가 중복된 갯수를 알기 위한 변수 선언 => count
+        5.가장 많은 중복 갯수를 알기위한 변수 선언 => max
+        6.가장 많이 중복된 갯수가 여러개인지 아닌지 알기위한 변수 선언 => result
+        7.가장 많은 중복 갯수를 알아 냈을때, 해당 문자를 대문자로 변환해서 저장할 변수 선언 => resultString
+        */
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine().toLowerCase();
-        String reverse = "";
+        String s = scanner.nextLine();
+        String ss = s.toUpperCase();
+        int[] arr = new int[26];
         int count = 0;
+        int max = 0;
+        int result = 0;
+        String resultString = null;
 
-        //입력받은 문자열을 뒤집어서 저장한다.
-        //문자열의 뒤부터 입력받아야하기 때문에 시작 : s.length() - 1 => index이기 때문에 -1 해 줌
-        for (int i = s.length() - 1; i >= 0; i--) {
-            reverse = reverse + s.charAt(i);
+        //알파벳 대문자 A~Z까지를 아스키코드로 변환하여 반복문 실행
+        for (int i = 65; i < 91; i++) {
+
+            //입력받은 문자열이 어떤 알파벳으로 구성 되어있는지 확인하기 위한 반복문 실행
+            for (int j = 0; j < s.length(); j++) {
+
+                //해당 문자가 일치하는 알파벳을 만났을때, arr[]에 알파벳 위치에 count 수 저장
+                //=>저장된 count(수) 는 max를 구할때 사용 될 예정
+                if (ss.charAt(j) == i) {
+                    count++;
+                    arr[i-65] = count;
+                }
+            }
+            count = 0;
         }
 
-        //문자열의 정방향과 역방향을 비교해서 팰린드롬인지 확인.
-        //각각의 위치의 문자가 일치하면 count의 수를 1씩 증가
-        //=> 이유? s.length() == count 인지 확인하기 위해서.
-        //=> (s.charAt(i) == reverse.charAt(i)) 거짓이면 반복문을 바로 탈출.
-        //=> 이유? 팰린드롬이 아니기 때문에 더 이상 진행할 필요X
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == reverse.charAt(i)) {
-                count += 1;
-            } else {
-                break;
+        //arr[]에서 가장 큰 값을 찾기 위한 반복문.
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+
+                //arr[]의 구조는 알파벳의 갯수 = 총 26
+                //=>아스키코드 구조 A~Z = 65~90
+                //=>즉, max < arr[i]을 만족할때, resultString에 저장될 문자를 구하는 식은
+                //=>i+65를 문자로 변환해 줘야한다.
+                resultString = String.valueOf((char)(i+65));
             }
         }
 
-        //입력받은 문자열이 팰린드롬인지 확인
-        //확인방법 : s.length() == count 인지
-        if (s.length() == count) {
-            System.out.println("1");
+        //max값이 arr[]에 일치하는게 몇개 인지 확인하기 위한 반복문
+        //=> arr[]에 max값은 최소 1개 있다.
+        //=> 하지만, result값이 2 이상이 된다면? max값이 중복이라는 의미
+        for (int i = 0; i < arr.length; i++) {
+            if (max == arr[i]) {
+                result += 1;
+            }
+        }
+
+        if (result > 1) {
+            System.out.println("?");
         } else {
-            System.out.println("0");
+            System.out.println(resultString);
         }
     }
 }
--- src/stage6/Stage6_5.java
+++ src/stage6/Stage6_5.java
@@ -1,5 +1,6 @@
 package stage6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Stage6_5 {
@@ -40,60 +41,60 @@ public class Stage6_5 {
         */
 
         /*
-        1. 문자열을 입력받는다.
-        2. 입력받은 문자열의 길이만큼의 int타입의 배열을 선언한다.
-        3. 입력받은 문자열에서 각각의 문자가 중복되는지 확인하기 위한 변수(count) 선언
-        4. 가장 많이 중복된 횟수를 저장하기 위한 변수(max) 선언
-        5. 가장 많이 중복된 횟수를 가진 문자가 몇개인지 확인하기 위한 변수(result) 선언
+        1.문자열을 입력받는다.
+        2.입력받은 문자열을 대문자로 전환.
+        3.배열의 크기가 26(알파벳 갯수)인 int타입의 배열 선언
+        4.문자열에 해당 문자가 중복된 갯수를 알기 위한 변수 선언 => count
+        5.가장 많은 중복 갯수를 알기위한 변수 선언 => max
+        6.가장 많이 중복된 갯수가 여러개인지 아닌지 알기위한 변수 선언 => result
+        7.가장 많은 중복 갯수를 알아 냈을때, 해당 문자를 대문자로 변환해서 저장할 변수 선언 => resultString
         */
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine().toLowerCase();
-        int[] arr = new int[s.length()];
-        String ss = null;
+        String s = scanner.nextLine();
+        String ss = s.toUpperCase();
+        int[] arr = new int[26];
         int count = 0;
         int max = 0;
         int result = 0;
+        String resultString = null;
 
-        if (s.length() <= 1000000) {
+        //알파벳 대문자 A~Z까지를 아스키코드로 변환하여 반복문 실행
+        for (int i = 65; i < 91; i++) {
 
-            //중첩 반목문을 활용해서 문자열을 각각의 문자로 나누어서 비교
-            for (int i = 0; i < s.length(); i++) {
-                for (int j = i + 1; j < s.length(); j++) {
+            //입력받은 문자열이 어떤 알파벳으로 구성 되어있는지 확인하기 위한 반복문 실행
+            for (int j = 0; j < s.length(); j++) {
 
-                    //똑같은 문자 발견시, count값을 1씩 증가
-                    //=> 그리고 int타입의 배열에 문자열의 위치에 count값 저장.
-                    if (s.charAt(i) == s.charAt(j)) {
-                        count += 1;
-                        arr[i] = count;
-                    }
-                }
-
-                //1개의 문자가 모든 문자들과 비교가 끝나면 count값 0으로 초기화
-                count = 0;
-            }
-
-            //가장 많이 중복된 단어와 단어의 위치를 알기위한 반복문
-            for (int i = 0; i < arr.length; i++) {
-
-                //max값을 0으로 설정해 놓고, 문자열의 각각의 문자위치의 중복값을 저장한 count의 값 비교
-                //높은수가 갱신 될 때마다, max에 저장. ss(문자를 알기위함)에 문자 저장
-                if (max < arr[i]) {
-                    max = arr[i];
-                    ss = String.valueOf(s.charAt(i));
+                //해당 문자가 일치하는 알파벳을 만났을때, arr[]에 알파벳 위치에 count 수 저장
+                //=>저장된 count(수) 는 max를 구할때 사용 될 예정
+                if (ss.charAt(j) == i) {
+                    count++;
+                    arr[i-65] = count;
                 }
             }
+            count = 0;
+        }
 
-            for (int i = 0; i < arr.length; i++) {
-                if (max == arr[i]) {
-                    result += 1;
-                }
+        //arr[]에서 가장 큰 값을 찾기 위한 반복문.
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                resultString = String.valueOf(ss.charAt(i));
             }
+        }
 
-            if (result > 1) {
-                System.out.println("?");
-            } else {
-                System.out.println(ss);
+        //max값이 arr[]에 일치하는게 몇개 인지 확인하기 위한 반복문
+        //=> arr[]에 max값은 최소 1개 있다.
+        //=> 하지만, result값이 2 이상이 된다면? max값이 중복이라는 의미
+        for (int i = 0; i < arr.length; i++) {
+            if (max == arr[i]) {
+                result += 1;
             }
+        }
+
+        if (result > 1) {
+            System.out.println("?");
+        } else {
+            System.out.println(resultString);
         }
     }
 }
